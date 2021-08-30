8d2c4cc1cb43070074bee86fa73d6a875d9587fc
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-08-29 21:19:07 -0400

    + Set up the character and the scene. + Create 2 scripts that handle the character's and the camera's movements.

---
+++ Homework 1 - Character Controller/Character Controller/Assets/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,53 @@
+﻿/**
+ * CameraMovement.cs
+ * Description: This script handles the movement of the player's camera.
+ * Programmer: Khoi Ho
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField] private GameObject CharacterMesh;
+    [SerializeField] private float SensitivityX = 100.0f;
+    [SerializeField] private float SensitivityY = 100.0f;
+
+    private Vector3 DistanceVector;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Hide and lock mouse cursor.
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+
+        DistanceVector = this.transform.position - CharacterMesh.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        RotateCamera();
+        MoveCamera();   
+    }
+
+    void MoveCamera()
+    {
+        // Keep the same distance from the camera to the character.
+        this.transform.position = CharacterMesh.transform.position + DistanceVector;
+
+        // Make sure that the camera looks at the character mesh.
+        transform.LookAt(CharacterMesh.transform);
+    }
+
+    void RotateCamera()
+    {
+        float MouseX = Input.GetAxis("Mouse X");
+        float MouseY = Input.GetAxis("Mouse Y");
+
+        Quaternion CameraRotation = Quaternion.AngleAxis(MouseX * SensitivityX * Time.deltaTime, CharacterMesh.transform.up);
+        DistanceVector = CameraRotation * DistanceVector;
+    }
+}
---
+++ Homework 1 - Character Controller/Character Controller/Assets/Scripts/Player/CharacterMovement.cs
@@ -0,0 +1,69 @@
+﻿/**
+ * CharacterMovement.cs
+ * Description: This script handles the movement of the player's mesh.
+ * Programmer: Khoi Ho
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    [SerializeField] private Rigidbody RigidBodyComponent;
+    [SerializeField] private Camera Camera;
+
+    [SerializeField] private float MoveSpeed = 3.0f; // How fast the character moves.
+    [SerializeField] private float TurnRate = 4.0f;           // How fast the character turns.
+
+
+    private bool bIsGrounded = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        bIsGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        bIsGrounded = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+
+    private void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    void HandleMovement()
+    {
+        // Get input.
+        float Vertical = Input.GetAxis("Vertical");
+        float Horizontal = Input.GetAxis("Horizontal");
+        
+        // Calculate the move direction relative to the character.
+        Vector3 LocalMoveDirection = new Vector3(Horizontal, 0.0f, Vertical).normalized;
+
+        if (LocalMoveDirection.magnitude >= 0.1f) // Prevent the character from turning when the player does not press any keys.
+        {
+            // Rotate the character.
+            float RotationAngleInDegrees = Mathf.Atan2(LocalMoveDirection.x, LocalMoveDirection.z) * Mathf.Rad2Deg + Camera.transform.eulerAngles.y;
+            RigidBodyComponent.rotation = Quaternion.Lerp(RigidBodyComponent.rotation, Quaternion.Euler(0.0f, RotationAngleInDegrees, 0.0f), Time.fixedDeltaTime * TurnRate);
+
+            // Move the character.
+            Vector3 WorldMoveDirection = Quaternion.Euler(0.0f, RotationAngleInDegrees, 0.0f) * Vector3.forward;
+            RigidBodyComponent.AddForce(WorldMoveDirection * MoveSpeed * Time.deltaTime, ForceMode.VelocityChange);
+        }
+    }
+}
