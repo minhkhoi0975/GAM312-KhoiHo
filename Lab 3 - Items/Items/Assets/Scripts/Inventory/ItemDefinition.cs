fdb6db6555a570eb7bf015e0419078efa4b6d51e
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-10-03 22:59:02 -0400

    Created class related to items: ItemDefinition, ItemInstance, Inventory.

---
+++ Lab 3 - Items/Items/Assets/Scripts/Inventory/ItemDefinition.cs
@@ -0,0 +1,54 @@
+/**
+ * ItemTemplate.cs
+ * Description: This script defines an item.
+ * Programmer: Khoi Ho
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType
+{
+    Armor = 1 << 0,
+    Weapon = 1 << 1
+}
+
+public class ItemDefinition : ScriptableObject
+{
+    public string name;
+
+    private int maxStackSize = 1;
+    public int MaxStackSize
+    {
+        get
+        {
+            return maxStackSize;
+        }
+        set
+        {
+            maxStackSize = value < 1 ? 1 : value;
+        }
+    }
+
+    ItemType type;
+
+    // Does this item belong to an item type?
+    bool IsOfType(ItemType itemType)
+    {
+        return (int)(type & itemType) != 0;
+    }
+
+    // Make the item to belong to or not belong to an item type.
+    void SetType(ItemType itemType, bool IsOfType = true)
+    {
+        if (IsOfType)
+        {
+            type = type | itemType;
+        }
+        else
+        {
+            type = type & ~itemType;
+        }
+    }
+}
---
+++ Lab 3 - Items/Items/Assets/Scripts/Inventory/ItemInstance.cs
@@ -0,0 +1,28 @@
+/**
+ * ItemInstance.cs
+ * Description: This script handles an instance of an item.
+ * Programmer: Khoi Ho
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemInstance
+{
+    // What is the definition of this item instance?
+    public ItemDefinition itemDefinition;
+
+    private int currentStackSize = 1;
+    public int CurrentStackSize
+    {
+        get
+        {
+            return currentStackSize;
+        }
+        set
+        {
+            currentStackSize = value < 1 ? 1 : (value > itemDefinition.MaxStackSize ? itemDefinition.MaxStackSize : value);  
+        }
+    }
+}
