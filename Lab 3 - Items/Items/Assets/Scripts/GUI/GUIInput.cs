89eb50fe8533b1c1f5bc75f4aeb2dbdebe7ac8e8
minhkhoi0975 <minhkhoi0975@gmail.com>
2021-10-19 00:29:18 -0400

    Added input to toggle/untoggle inventory UI.

--- Lab 3 - Items/Items/Assets/Scripts/GUI/GUIInput.cs
+++ Lab 3 - Items/Items/Assets/Scripts/GUI/GUIInput.cs
@@ -11,17 +11,24 @@ using UnityEngine.UI;
 
 public class GUIInput : MonoBehaviour
 {
-    
+    public GameObject inventoryPanel;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        if(!inventoryPanel)
+        {
+            inventoryPanel = FindObjectOfType<InventoryGUILogic>().gameObject;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Open/Close the inventory.
+        if(Input.GetButtonDown("Inventory"))
+        {
+            inventoryPanel.SetActive(!inventoryPanel.activeSelf);
+        }
     }
 }
